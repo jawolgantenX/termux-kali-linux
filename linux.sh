fed2e6747911d88b978f3519f986d4580d463ec4
jawolgantenX <77243185+jawolgantenX@users.noreply.github.com>
2021-02-26 20:44:09 +0700

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,29 @@
+#usr/bin/bash
+#anda lagi 
+#iya saya lagi
+#anda lagi
+#iya saya lagi
+
+echo "kamu jelek buriq"
+sleep 3
+echo " kamu gk glowing "
+sleep 2
+echo "kamu miskin"
+sleep 3
+
+echo
+echo
+echo
+echo "di atas cuman pengertian"
+echo " momen ketika lu di tolak nembak cewek"
+echo "wkwkwkwk wkwkwkwkwk"
+ echo
+ echo
+ echo
+ echo
+ echo
+ 
+ 
+ 
+ wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+./start-kali.sh
